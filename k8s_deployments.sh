4ddcf632a99e75385c3107e656bf79c58256731f
Jin Huang <huangjinzhuo@gmail.com>
2020-04-20 21:39:32 -0700

    rolling update

--- k8s_deployments.sh
+++ k8s_deployments.sh
@@ -1,25 +1,51 @@
-RollingUpdate, Canary, Blue/Green Deployments on Kubernetes
+# RollingUpdate, Canary, Blue/Green Deployments on Kubernetes
+
 
 git clone https://github.com/googlecodelabs/orchestrate-with-kubernetes.git
 cd orchestrate-with-kubernetes/kubernetes
 
-check out deployments/frontend.yaml file for configMap example
-spec:
-  containers:
-    volumeMounts:
-      - name: xxxx
-        mountPath: xxxxxx
-      -name: "tls-certs"
-   volumes:
-        - name: xxxxxxx
-          secret:
-            secretName: xxxx
-        - name: "nginx-frontend-conf"
-          configMap:
-            name: "nginx-frontend-conf"
-            items:
-              - key: "frontend.conf"
-                 path: "frontend.conf"
+gcloud config set compute/zone us-central1-a
+gcloud container clusters create bootcamp --num-nodes 5 --scopes "https://www.googleapis.com/auth/source.read_write,storage-rw"
+
+kubectl explain deployment
+kubectl explain deployment --recursive
+kubectl explain deployment.metadata.name
+
+sudo sed "s/auth:1.0.0/auth:2.0.0/g" deployments/auth.yaml
+
+kubectl create -f deployments/auth.yaml
+kubectl create -f services/auth.yaml
+
+kubectl create -f deployments/hello.yaml
+kubectl create -f services/hello.yaml
+
+kubectl get deployments
+kubectl get replicasets
+kubectl get pods
+
+
+# check out deployments/frontend.yaml file for configMap example
+# spec:
+#   containers:
+#     volumeMounts:
+#       - name: xxxx
+#         mountPath: xxxxxx
+#       -name: "tls-certs"
+#    volumes:
+#         - name: xxxxxxx
+#           secret:
+#             secretName: xxxx
+#         - name: "nginx-frontend-conf"
+#           configMap:
+#             name: "nginx-frontend-conf"
+#             items:
+#               - key: "frontend.conf"
+#                  path: "frontend.conf"
+
+kubectl create secret generic tls-certs --from-file tls/
+kubectl create configmap nginx-frontend-conf --from-file=nginx/frontend.conf
+kubectl create -f deployments/frontend.yaml
+kubectl create -f services/frontend.yaml
 
 curl http://`kubectl get service frontend -o jsonpath="{.status.loadBalancer.ingress[0].ip}"`
 
