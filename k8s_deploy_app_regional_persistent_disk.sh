b04f84e98bae9cb38dbfb233ae1f520827e88d37
Jin Huang <huangjinzhuo@gmail.com>
2020-04-24 21:03:23 -0700

    init

---
+++ k8s_deploy_app_regional_persistent_disk.sh
@@ -0,0 +1,7 @@
+#### Deploy applications with Regional Persistant Disks, in multi-zone Kubernetes.
+#### Each zone has Kubernetes Master(s) and Nodes
+
+# key concept: Kebernetes StorageClass resource across replicated zones
+# deleteing a node won't bring down application
+
+
