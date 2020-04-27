8438d6e9e005c00821d130eeedac717fecbf0f4f
Jin Huang <huangjinzhuo@gmail.com>
2020-04-26 18:43:02 -0700

    discover endpoints

--- k8s_memcached.sh
+++ k8s_memcached.sh
@@ -25,3 +25,40 @@ helm repo update
 
 # Install memcached Helm chat
 helm install stable/memcached --name mycache --set replicaCount=3
+
+# discovering memcached endpoints
+kubectl get services mycache-memcached -o jsonpath="{.spec.clusterIP}" ; echo
+# output None because the service was deployed as a headless(no clusterIP) service.
+
+# retrieve the endpoints' IP addresses with one of the following two lines
+kubectl get endpoints mycache-memcached
+kubectl run -it alpine --rm --image=alpine:3.6 --restart=Never -- nslookup mycache-memcached.default.svc.cluster.local
+
+# test the deployment by running a telnet on port 11211
+kubectl run -it alpine --rm --image=alpine:3.6 --restart=Never \
+telnet mycache-memcached-0.mycache-memcached.default.svc.cluster.local 11211
+# inside telnet prompt:
+    #   store a key
+        #   set mykey 0 0 5
+        #   hello
+        #   get mykey
+    #   VALUE mykey 0 5
+        #   hello
+        #   END
+        #   quit
+
+# use endpoints in Python. (need pymemcache library)
+# start a shell and install pymemcache
+kubectl run -it alpine --rm --image=alpine:3.6 --restart=Never sh
+pip install pymemcache
+python
+# run the following in Python console(>>>)
+    # import socket
+    # from pymemcache.client.hash import HashClient
+    # _, _, ips = socket.gethostbyname_ex('mycache-memcached.default.svc.cluster.local')
+    # servers = [(ip, 11211) for ip in ips]
+    # client = HashClient(servers, use_pooling=True)
+    # client.set('mykey', 'hello')
+    # client.get('mykey')
+# output is b'hello'
+    # exit()
