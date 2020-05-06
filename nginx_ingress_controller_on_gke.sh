e9623821efec16d41d92cbbb7758193647417244
Jin Huang <huangjinzhuo@gmail.com>
2020-05-06 16:17:52 -0700

    nginx

---
+++ nginx_ingress_controller_on_gke.sh
@@ -0,0 +1,12 @@
+#### NGINX Ingress Controller on GKE
+#### NGINX advantages: Websockets, SSL Services, Session Persistence, JWTs validation.
+
+
+# Deploy NGINX Ingress Controller using a Helm Chart
+gcloud config set compute/zone us-central1-a
+
+# Deploy an Ingress Resource for apps to use NGINX Ingress as a Controller
+
+# Deploy a web app
+
+# Test NGINX Ingress. Using a GCP L4 TCP/UDP Load Balancer frontend IP, and ensure it can access the web app.
